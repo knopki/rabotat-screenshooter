b8fb7fe9a18f07b77ff9cd7f942a3f6b18114c7d
Sergey Korolev <korolev.srg@outlook.com>
2015-11-18 11:34:46 +0300

    Create directories

--- Program.cs
+++ Program.cs
@@ -7,19 +7,23 @@ namespace rabotat_screenshooter
 {
     class Program
     {
-        static public string path = "";
+        static public string path = @"";
         static void Main(string[] args)
         {
-            
             if (args.Length > 0) {
                 path = args[0];
             }
 
+            path = Path.Combine(path, Environment.UserName.ToLower() + "@" + Environment.UserDomainName.ToLower());
+            path = Path.Combine(path, DateTime.Now.ToString("yyyy-MM-dd"));
+
+            DirectoryInfo di = Directory.CreateDirectory(path);
+
             var imageName = Path.Combine(path,
-                Environment.UserName.ToLower() + "_" +
+                Environment.UserName.ToLower() + "@" +
                 Environment.UserDomainName.ToLower() + "_" +
-                Environment.MachineName.ToLower() + "_" +
-                DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss") + ".png");
+                DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss") + "_" +
+                Environment.MachineName.ToLower() + ".png");
 
             Image screen = Pranas.ScreenshotCapture.TakeScreenshot();
             screen.Save(imageName, ImageFormat.Png);
