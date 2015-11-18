1d956af5d6b4641d2f4732b994cb12c7b3e699f0
Sergey Korolev <korolev.srg@outlook.com>
2015-11-18 03:06:42 +0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Diagnostics;
+
+namespace rabotat_screenshooter
+{
+    class Program
+    {
+        static public string path = "";
+        static void Main(string[] args)
+        {
+            
+            if (args.Length > 0) {
+                path = args[0];
+            }
+
+            var imageName = Path.Combine(path,
+                Environment.UserName.ToLower() + "_" +
+                Environment.UserDomainName.ToLower() + "_" +
+                Environment.MachineName.ToLower() + "_" +
+                DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss") + ".png");
+
+            Image screen = Pranas.ScreenshotCapture.TakeScreenshot();
+            screen.Save(imageName, ImageFormat.Png);
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("rabotat-screenshooter")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("rabotat-screenshooter")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("9272bb04-3610-46cc-8408-8d4bb314fe89")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
